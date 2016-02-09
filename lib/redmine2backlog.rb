0771745585b5f7feec43abb8cc102b6239b21816
Kyohei Hamada <kyohei.hamada@gmail.com>
2016-02-09 18:44:13 +0900

    wiki migration

--- lib/redmine2backlog.rb
+++ lib/redmine2backlog.rb
@@ -3,16 +3,50 @@ require "redmine2backlog/version"
 require 'faraday'
 require 'json'
 
-module Redmine2backlog
+require 'backlog_kit'
+require 'pandoc-ruby'
+
+class Redmine2backlog
+  attr_reader :opts, :redmine, :backlog
+
   def initialize opts
-    @opts = opts.except(:redmine, :backlog)
+    @opts = opts
     @redmine = Redmine.new(opts[:redmine])
+    @backlog = Backlog.new(opts[:backlog])
   end
 
-  def export
-    @opts[:redmine][:projects]
+  def redmine_wikis
+    @redmine_wikis ||= redmine.projects.map do |project|
+      project.wikis["wiki_pages"].map do |wiki|
+        project.wikis(wiki["title"])
+      end
+    end.flatten
   end
 
+  def import_wikis
+    redmine_wikis.each do |wiki|
+      import_wiki(wiki["wiki_page"]['title'], wiki["wiki_page"]['text'])
+    end
+  end
+
+  private
+
+  def import_wiki title, content
+    backlog.client.post('wikis', {
+      projectId: backlog.project_id,
+      name: title,
+      content: convert(content)
+    })
+  rescue => e
+    puts "rename '#{title}' --> '#{title} : dup'"
+    if e.message.match(/Name already exist/)
+      import_wiki "#{title} : dup", content
+    end
+  end
+
+  def convert text
+    PandocRuby.convert(text, {:f => :textile, :to => :markdown}).gsub(/\\\[\\\[(.*)\\\]\\\]/, '[[\1]]')
+  end
 end
 
 class Redmine
@@ -54,6 +88,30 @@ class Redmine
     def initialize raw
       @raw = raw
       @body = JSON.parse(raw.body)
+    end
+  end
+end
+
+class Backlog
+  attr_reader :opts, :client
+
+  def initialize opts
+    @opts = opts
+
+    @client = BacklogKit::Client.new(space_id: opts[:space_id], api_key: opts[:key])
+  end
+
+  def project
+    @project ||= client.get_project(opts[:project])
+  end
+
+  def project_id
+    @project_id ||= project.body.id
+  end
+
+  def delete_all_wikis
+    client.get_wikis(project_id).body.map do |w|
+      client.delete_wiki(w.id) if w.name != 'Home'
     end
   end
 end
--- lib/redmine2backlog/version.rb
+++ lib/redmine2backlog/version.rb
@@ -1,3 +1,3 @@
-module Redmine2backlog
+class Redmine2backlog
   VERSION = "0.1.0"
 end
